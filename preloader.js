be08211407c565a0ff8ac8036d1d6624f6a26f5d
thefuzzycreature <153345316+thefuzzycreature@users.noreply.github.com>
2024-05-04 00:49:33 -0700

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -16,7 +16,7 @@ overlay.style.cssText = `
 
 const svgImage = document.createElement('img');
 svgImage.id = 'svgImage';
-svgImage.src = 'https://jvsc99.github.io/preloaderJs/imagem.svg';
+svgImage.src = 'https://thefuzzycreature.github.io/preloaderJs/imagem.svg';
 svgImage.style.cssText = `
     max-width: 100%;
     max-height: 100%;
